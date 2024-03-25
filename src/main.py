da51cf03bfc1d0e4dd57252d3dca763648c28dd0
EtomicBomb <ethan@ethan.ws>
2024-03-25 03:29:03 -0400

    fixed bug in main about paths and some other ones

--- src/main.py
+++ src/main.py
@@ -8,23 +8,17 @@ from itertools import takewhile
 from pathlib import Path
 import json
 
-# no, this doesn't work
-# E external/local_xla/xla/stream_executor/cuda/cuda_driver.cc:274] failed call to cuInit: CUDA_ERROR_NO_DEVICE: no CUDA-capable device is detected
-# F external/local_tsl/tsl/platform/default/env.cc:74] Check failed: ret == 0 (11 vs. 0)Thread tf_data_private_threadpool creation via pthread_create() failed.
-
-
 @tf.py_function(Tout=(tf.TensorSpec(shape=(None, 224, 224), dtype=tf.uint8), tf.int32))
 def fetch_segment(path, lo, hi, label): 
-    print(path, lo, hi, label)
+    path, lo, hi, label = path.numpy().decode('utf-8'), int(lo.numpy()), int(hi.numpy()), label
     with av.open(path) as container:
         stream, = container.streams
-        container.seek(lo, backwards=True, any_frame=False, stream=stream)
-        undecoded = takewhile(frame.pts < hi, container.decode(stream)) 
-        return tf.stack([frame.to_ndarray() for frame in undecoded]), label
+        container.seek(lo, backward=True, any_frame=False, stream=stream)
+        undecoded = takewhile(lambda frame: frame.pts < hi, container.decode(stream)) 
+        return tf.stack([tf.constant(frame.to_ndarray()) for frame in undecoded]), label
 
 def process_file(path):
     for path in Path('data/extract', path).glob('*'):
-        print(path)
         path = path.with_suffix('.mp4')
         label, *_ = path.with_suffix('').name.split('_')
         label = int(label)
@@ -35,7 +29,7 @@ def process_file(path):
             lo = keyframes[::keyframes_per_example][:-1]
             hi = keyframes[::keyframes_per_example][1:]
             for lo, hi in zip(lo, hi):
-                yield path, lo, hi, label
+                yield str(path), lo, hi, label
 
 def get_dataset(paths):
     datasets = {0: [], 5: [], 10: []}
@@ -46,7 +40,7 @@ def get_dataset(paths):
 
     sample_from = []
     for label, d in datasets.items():
-        d = Dataset.from_generator(
+        e = Dataset.from_generator(
             lambda: d,
             output_signature=(
                 tf.TensorSpec((), tf.string),
@@ -56,15 +50,14 @@ def get_dataset(paths):
             ),
         )
 
-        d = d.shuffle(d.cardinality(), reshuffle_each_iteration=True)
-        d.map(fetch_segment)
-        sample_from.append(d)
+        e = e.shuffle(e.cardinality(), reshuffle_each_iteration=True)
+        e = e.map(fetch_segment)
+        sample_from.append(e)
 
-    d = Dataset.sample_from_datasets(
+    return Dataset.sample_from_datasets(
         sample_from,
         rerandomize_each_iteration=True,
     )
-    return d
 
 splits = json.loads(Path('data/split.json').read_text())
 
@@ -72,5 +65,5 @@ splits = json.loads(Path('data/split.json').read_text())
 # test = get_dataset(splits['test']).batch(8).prefetch(2)
 # validation = get_dataset(splits['validation']).batch(8).prefetch(2)
 
-for x in get_dataset(splits['train']).as_numpy_iterator():
-    print(x)
+for x, y in get_dataset(splits['train'][:4]).as_numpy_iterator():
+    print(x, y)
