c2a92b2000d2dc588338783a4cf69207b719d42f
EtomicBomb <ethan@ethan.ws>
2024-05-05 07:07:38 -0400

    experiment

--- src/main.py
+++ src/main.py
@@ -34,7 +34,6 @@ def get_data(
     if mode == 'train':
         data = data.repeat()
     data = data.map(lambda pts, path, label: (pts, tf.strings.join([extract_root, path]), label))
-    print(data.cardinality())
     data = get_dataset(data, s=src_shape)
 
     model = keras.Sequential([
@@ -113,8 +112,97 @@ def demo():
     ani = FuncAnimation(fig, animate, data, cache_frame_data=False, blit=True, interval=1)
     plt.show()
 
+class VideoMobileNet(keras.layers.Layer):
+    def __init__(self, *args, start=1, end=None, trainable=True, **kwargs):
+        super().__init__(*args, **kwargs)
+        model = keras.applications.MobileNetV2(weights='imagenet', input_shape=(224, 224, 3))
+        start = model.get_layer(start)
+        end = model.get_layer(end)
+        model = keras.Model(inputs=start.output,outputs=end.output)
+        for layer in model.layers:
+           layer.trainable = trainable
+        self.model = model
+    def call(self, x):
+        batch_size, frame_count, height, width, channels = x.shape
+        x = tf.reshape(x, (batch_size*frame_count, height, width, channels))
+        x = self.model(x)
+        _, height, width, channels = x.shape
+        x = tf.reshape(x, (batch_size, frame_count, height, width, channels))
+        return x
+
+class Video1DConvolution(keras.layers.Layer):
+    def __init__(self, filters, kernel_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.m = keras.layers.Conv1D(filters, kernel_size, padding='same', data_format='channels_first')
+    def call(self, x):
+        batch_size, frame_count, height, width, channels = x.shape
+        x = tf.reshape(x, (batch_size, frame_count, height * width * channels), name='bar2')
+        x = self.m(x)
+        x = tf.reshape(x, (batch_size, x.shape[1], height, width, channels), name='foo2')
+        return x
+
+def experiment():
+    # https://keras.io/api/applications/
+    s = Dimensions(
+        batch_size=4,
+        frame_count=32,
+        height=224,
+        width=224,
+        channels=3,
+    )
+
+    model = keras.Sequential([
+        keras.Input(shape=s.example_shape, batch_size=s.batch_size),
+        tf.keras.layers.Rescaling(2.0, -1.0), # [0,1] -> [-1, 1]
+        VideoMobileNet(start='input_layer_1',end='block_3_depthwise', trainable=False),
+        Video1DConvolution(32, 20),
+        VideoMobileNet(start='block_3_depthwise',end='block_6_depthwise', trainable=False),
+        Video1DConvolution(32, 20),
+        VideoMobileNet(start='block_6_depthwise',end='block_13_depthwise', trainable=False),
+        Video1DConvolution(32, 20),
+        VideoMobileNet(start='block_13_depthwise',end='block_16_expand', trainable=True),
+        Video1DConvolution(32, 20),
+        VideoMobileNet(start='block_16_expand',end='out_relu', trainable=True),
+        Video1DConvolution(32, 20),
+        keras.layers.Conv3D(1, (10, 3, 3), strides=(3, 3, 3), activation='relu'),
+        keras.layers.Flatten(),
+        keras.layers.Dense(3, activation='sigmoid'),
+    ])
+
+    train_data = get_data(
+        mode='train',
+        extract_root='data/extract/',
+        data_root='data',
+        batch_size=3,
+        frame_count=32,
+    )
+
+    test_data = get_data(
+        mode='test',
+        extract_root='data/extract/',
+        data_root='data',
+        batch_size=3,
+        frame_count=32,
+    )
+
+    model.compile(
+        optimizer=keras.optimizers.AdamW(
+          learning_rate=1e-7
+        ),
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+        metrics=[keras.metrics.SparseCategoricalAccuracy()],
+    )
+    history = model.fit(
+        train_data,
+        steps_per_epoch=20,
+        epochs=30, 
+    )
+    print(history)
+
+    model.summary()
+
 if __name__ == '__main__':
-    modes = dict(demo=demo, index=index)
+    modes = dict(demo=demo, index=index, experiment=experiment)
     parser = ArgumentParser(
         prog='drowsiness classifier',
         description='sees if someone is drowsy',
