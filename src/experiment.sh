94cd9a896a057ac3e832c338a689bb9379b36b4d
EtomicBomb <ethan@ethan.ws>
2024-05-06 00:08:46 -0400

    changed time limit

--- src/experiment.sh
+++ src/experiment.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1  
 #SBATCH --mem-per-cpu=16G   
-#SBATCH -t 10:00:00
+#SBATCH -t 5:00:00
 #SBATCH --mail-type=begin     
 #SBATCH --mail-type=end       
 #SBATCH --mail-user=ethan_williams@brown.edu
@@ -13,4 +13,4 @@ module purge
 unset LD_LIBRARY_PATH
 srun apptainer exec --nv tensorflow-24.03-tf2-py3.simg python3 src/main.py $@
 
-# bash src/experiment.sh --mode experiment --run-id hello --exclude noise --learning-rate 1e-7
+# srun src/experiment.sh -- --mode experiment --run-id hello --exclude noise --learning-rate 1e-7
