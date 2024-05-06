42027af116c4caff5cdc40414695d8e44cb3b681
EtomicBomb <ethan@ethan.ws>
2024-05-06 17:16:40 -0400

    quote args

--- src/experiment.sh
+++ src/experiment.sh
@@ -11,6 +11,6 @@
 
 module purge
 unset LD_LIBRARY_PATH
-srun apptainer exec --nv /oscar/runtime/software/external/ngc-containers/tensorflow.d/x86_64.d/tensorflow-24.03-tf2-py3.simg python3 src/main.py $@
+srun apptainer exec --nv /oscar/runtime/software/external/ngc-containers/tensorflow.d/x86_64.d/tensorflow-24.03-tf2-py3.simg python3 src/main.py "$@"
 
 # srun src/experiment.sh -- --mode experiment --run-id hello --exclude noise --learning-rate 1e-7
